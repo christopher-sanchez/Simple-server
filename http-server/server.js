f368ca8249ce9a91001df9e208607c24fef44ed2
christopher-sanchez <csanchez@my.okcu.edu>
2026-03-12 16:38:25 -0500

    setting structure and step 1

---
+++ http-server/server.js
@@ -0,0 +1,60 @@
+import http from 'http';
+
+const PORT = 3000;
+
+// step 1
+
+const server = http.createServer((req, res) => { 
+    console.log(`${req.method} ${req.url}`);
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end('<h1>Hello World</h1>');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+
+/*
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/' && method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Home Page</h1>');
+    }
+    else if (url === '/about' && method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>About Page</h1>');
+    }
+    else if(url ==='/api/data' && method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({message:'Hello',timestamp: Date.now()}));
+    }
+    else{
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Not Found</h1>');
+    }
+});
+
+import http from 'http';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+async function serveFile(filePath,contentType, res) {
+    try {
+        const data = await fs.readFile(filePath);
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data);
+    } catch (error) {
+        res.wrtiteHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Not Found</h1>');
+    }
+}
+*/
